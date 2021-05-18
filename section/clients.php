0806d459b889d5b664a9697b82f736f9526a1359
mrinal <mrinalhaque99@gmail.com>
2021-05-18 20:51:30 +0600

    git repo init

--- section/clients.php
+++ section/clients.php
@@ -2,14 +2,14 @@
 if ( have_rows('client') ) {
     ?>
     <div class="container bg-clients m-0 mt-5 mb-5 pb-1 pt-1 mw-100">
-        <div class="row p-md-5 p-sm-2 m-4 m-sm-0">
+        <div class="row m-4 m-sm-0 pt-sm-0 pt-5 pb-sm-0 pb-3">
     <?php
     while ( have_rows('client') ) {
         the_row();
         $logo = get_sub_field('logo');
         $logo_url = wp_get_attachment_image_src( $logo['ID'], 'full' )[0];
         ?>
-        <div class="col-md-3 position-relative d-flex justify-content-center align-items-center">
+        <div class="col-md-3 col-6 position-relative d-flex justify-content-center align-items-center pb-sm-5 pb-5 pe-sm-3 pt-sm-5">
             <div class="img-wrapper">
                 <img src="<?php echo esc_attr( $logo_url ); ?>" alt="" class="img-fluid">
             </div>
